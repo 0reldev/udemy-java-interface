0e3900e423808d25bee37c8d11fe6a2f6863f43c
Aurélien Lonni <aurelien.lonni@gmail.com>
2023-06-26 17:06:13 +0200

    Lesson part 2

--- src/main/java/dev/lpa/FlightEnabled.java
+++ src/main/java/dev/lpa/FlightEnabled.java
@@ -2,6 +2,10 @@ package dev.lpa;
 
 public interface FlightEnabled {
 
+    double MILES_TO_KM = 1.609_34;
+
+    double KM_TO_MILES = 0.621371;
+
     void takeOff();
 
     void land();
--- src/main/java/dev/lpa/Main.java
+++ src/main/java/dev/lpa/Main.java
@@ -10,17 +10,34 @@ public class Main {
         Trackable tracked = bird;
 
         animal.move();  // Flaps wings
-//        flier.move();
-//        tracked.move();
 
-        flier.takeOff();
+        inFlight(flier);
 //        Bird is taking off
-        flier.fly();
 //        Bird is flying
-        tracked.track();
 //        Bird's coordinates recorded
-        flier.land();
 //        Bird is landing
 
+        inFlight(new Jet());
+//        Jet is taking off
+//        Jet is flying
+//        Jet's coordinates recorded
+//        Jet is landing
+
+        Trackable truck = new Truck();
+        truck.track();
+//        Truck's coordinates recorded
+
+        double kmsTraveled = 100;
+        double milesTraveled = kmsTraveled * FlightEnabled.KM_TO_MILES;
+        System.out.printf("The truck traveled %.2f km or %.2f miles%n", kmsTraveled, milesTraveled);
+    }
+
+    private static void inFlight(FlightEnabled flier) {
+        flier.takeOff();
+        flier.fly();
+        if (flier instanceof Trackable tracked) {
+            tracked.track();
+        }
+        flier.land();
     }
 }
